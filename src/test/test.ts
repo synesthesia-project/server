25195ff03e100c47bd87b7e485acff3189ac5a79
Sam Lanning <sam@samlanning.com>
2019-01-26 20:30:09 -0800

    fix unit tests

--- src/test/test.ts
+++ src/test/test.ts
@@ -2,6 +2,6 @@ import { expect } from 'chai';
 
 describe('Group 1', function() {
     it('some test', function() {
-      expect('hello').to.equal('bye');
+      expect('hello').to.equal('hello');
     });
 });
