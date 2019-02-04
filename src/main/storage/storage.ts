54cb21bd0330eb6deadf47b85df4f1c3eb9bca6b
Sam Lanning <sam@samlanning.com>
2019-02-03 18:48:14 -0800

    Introduce API for storage, and add initial tests

--- src/main/storage/storage.ts
+++ src/main/storage/storage.ts
@@ -1,9 +1,16 @@
-// import { PlayStateData as ControllerPlayStateData } from '@synesthesia-project/core/protocols/control/messages';
-// import { CueFile } from '@synesthesia-project/core/file/index';
+import { CueFile } from '@synesthesia-project/core/file/index';
 
 /**
- * This class will evenually store its files, but fow now we'll just keep them in memory
+ * This class will evenually store its files, but for now we'll just keep them in memory
  */
 export class Storage {
+
+  public async getFile(_id: string): Promise<CueFile> {
+    throw new Error('not implemented');
+  }
+
+  public async saveFile(_id: string, _file: CueFile): Promise<void> {
+    throw new Error('not implemented');
+  }
 
 }
---
+++ src/test/storage/storage.test.ts
@@ -0,0 +1,47 @@
+import { expect } from 'chai';
+import { CueFile } from '@synesthesia-project/core/file/index';
+
+import { Storage } from '../../main/storage/storage';
+
+/**
+ * Return a new promise that fails if the given promise does not fail in the correct manner
+ * @param promise the promise that should fail
+ * @param errorMsg the error message that should be given in the rejection
+ */
+function promiseError(promise: Promise<{}>, errorMsg: string): Promise<void> {
+  return promise
+        .then(() => { throw new Error('Not supposed to succeed'); })
+        .catch((err: Error) => {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(err.message).to.equal(errorMsg);
+        });
+}
+
+describe('Storage', () => {
+
+  describe('getFile()', () => {
+
+    it('Non-Existant', async () => {
+      const s = new Storage();
+
+      await promiseError(s.getFile('non-existent-id'), 'file not found');
+    });
+
+    it('Set And Get', async () => {
+      const id = '3fjc94jst';
+      const s = new Storage();
+      const f: CueFile = {
+        lengthMillis: 12345,
+        layers: []
+      };
+
+      await s.saveFile(id, f);
+
+      // Check invalid ID does not return file
+      await promiseError(s.getFile('non-existent-id'), 'file not found');
+      // Check file is correctly returned
+      expect(await s.getFile(id)).to.deep.equal(f);
+    });
+
+  });
+});
